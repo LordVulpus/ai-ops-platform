6a6ec6e5d097df1cdb4778b3f0fbb353962ead54
Ubuntu <azureuser@az-centralcomputer.rgdxuofxtd1e1o2wsy2bq1efvg.cwx.internal.cloudapp.net>
2026-03-02 16:12:35 +0000

    Updated app with API key

--- app/main.py
+++ app/main.py
@@ -1,18 +1,39 @@
 import logging
-from fastapi import FastAPI
+import os
+import numpy as np
+from fastapi import FastAPI, Header, HTTPException
+from sklearn.ensemble import IsolationForest
 from model import detect_anomaly
 
-logging.basicConfig(level=logging.INFO)
 
+logging.basicConfig(level=logging.INFO)
+model = IsolationForest(contamination=0.2)
 app = FastAPI()
+API_KEY = os.getenv("API_Key")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
 @app.post("/predict")
-def predict(data: dict):
-    logging.info(f"Received data: {data}")
-    values = data.get("values", [])
-    anomalies = detect_anomaly(values)
-    return {"anomalies": anomalies}
+def predict(data: dict, x_api_key: str = Header(None)):
+	# Check Security
+	if x_api_key != API_KEY:
+		raise HTTPException(status_code=403, detail="Forbidden")
+
+	logging.info(f"Received data: {data}")
+	values = data.get("values", [])
+
+	if not values:
+		return {"error": "No values provided"}
+
+	logging.info(f"Values extracted: {values}")
+
+	anomalies = detect(values)
+	return {"anomalies": anomalies}
+
+def detect(values):
+	X = np.array(values).reshape(-1, 1)
+	preds = model.fit_predict(X)
+	anomalies = [v for v, p in zip(values, preds) if p == -1]
+	return anomalies
